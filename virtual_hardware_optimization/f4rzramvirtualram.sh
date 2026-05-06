1f98c115ea53360bba91b12e5558040a447f12a6
Constdev <theprocramer@gmail.com>
2026-05-06 21:58:00 +0400

    Create f4rzramvirtualram.sh

---
+++ virtual_hardware_optimization/f4rzramvirtualram.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]; then 
+  echo "ERROR: "
+  exit 1
+fi
+
+echo "Virtual Ram Creating..."
+swapoff /dev/zram0 2>/dev/null
+echo 1 > /sys/block/zram0/reset 2>/dev/null
+
+modprobe zram
+if grep -q zstd /sys/block/zram0/comp_algorithm; then
+    echo zstd > /sys/block/zram0/comp_algorithm
+elif grep -q lzo-rle /sys/block/zram0/comp_algorithm; then
+    echo lzo-rle > /sys/block/zram0/comp_algorithm
+else
+    echo lzo > /sys/block/zram0/comp_algorithm
+fi
+echo 1536M > /sys/block/zram0/disksize
+mkswap /dev/zram0
+swapon -p 100 /dev/zram0
+
+echo "--- Successful! ---"
+echo "new ram:"
+zramctl
